43680a53648bc2bcc4a45a540790062b5df907e8
Alexsmobsenyojer <trejbal.jakub@email.cz>
2021-12-15 16:01:48 +0100

    a

--- nette/web-project/app/Bootstrap.php
+++ nette/web-project/app/Bootstrap.php
@@ -27,7 +27,7 @@ class Bootstrap
 		$configurator->addConfig($appDir . '/config/common.neon');
 		$configurator->addConfig($appDir . '/config/services.neon');
 		$configurator->addConfig($appDir . '/config/local.neon');
-		$configurator->setDebugMode(false);
+		$configurator->setDebugMode(true);
 		$configurator->enableTracy($appDir . '/log');
 		return $configurator;
 	}
