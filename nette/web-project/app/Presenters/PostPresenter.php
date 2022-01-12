8f81bfa9e80feca1e4b2fe32a708dbe1f6f53d3c
Alexsmobsenyojer <trejbal.jakub@email.cz>
2022-01-12 11:04:23 +0100

    COMMENTS

--- nette/web-project/app/Presenters/PostPresenter.php
+++ nette/web-project/app/Presenters/PostPresenter.php
@@ -8,13 +8,13 @@ final class PostPresenter extends Nette\Application\UI\Presenter
 {
 	private Nette\Database\Explorer $database;
 
-	public function __construct(Nette\Database\Explorer $database)
+public function __construct(Nette\Database\Explorer $database)
 	{
 		$this->database = $database;
 	}
 
-	public function renderShow(int $postId): void
-{
+public function renderShow(int $postId): void
+    {
 	$post = $this->database
 		->table('posts')
 		->get($postId);
@@ -23,5 +23,42 @@ final class PostPresenter extends Nette\Application\UI\Presenter
 	}
 
 	$this->template->post = $post;
-}
+	$this->template->comments = $post->related('comments')->order('created_at');
+    }
+
+	
+	
+    
+
+protected function createComponentCommentForm(): Form
+    {
+	$form = new Form; // means Nette\Application\UI\Form
+
+	$form->addText('name', 'Jméno:')
+		->setRequired();
+
+	$form->addEmail('email', 'E-mail:');
+
+	$form->addTextArea('content', 'Komentář:')
+		->setRequired();
+
+	$form->addSubmit('send', 'Publikovat komentář');
+    $form->onSuccess[] = [$this, 'commentFormSucceeded'];
+	return $form;
+    }
+
+public function commentFormSucceeded(\stdClass $data): void
+    {
+	$postId = $this->getParameter('postId');
+
+	$this->database->table('comments')->insert([
+		'post_id' => $postId,
+		'name' => $data->name,
+		'email' => $data->email,
+		'content' => $data->content,
+	]);
+
+	$this->flashMessage('Děkuji za komentář', 'success');
+	$this->redirect('this');
+    }
 }
