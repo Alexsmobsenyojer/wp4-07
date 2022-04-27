668f981fd4a4bad5d901b2fb7f653a9275cbec8e
Alexsmobsenyojer <trejbal.jakub@email.cz>
2022-04-27 14:55:44 +0200

    Registrace

--- nette/web-project/app/Presenters/SignPresenter.php
+++ nette/web-project/app/Presenters/SignPresenter.php
@@ -3,11 +3,16 @@ namespace App\Presenters;
 
 use Nette;
 use Nette\Application\UI\Form;
-
+use App\Model\UserFacade;
 
 final class SignPresenter extends Nette\Application\UI\Presenter
 {
-protected function createComponentSignInForm(): Form
+	private UserFacade $userfacade;
+	public function __construct(UserFacade $userfacade)
+	{
+		$this->userfacade = $userfacade;
+	}
+	protected function createComponentSignInForm(): Form
 	{
 		$form = new Form;
 		$form->addText('username', 'Kultistická přezdívka:')
@@ -21,20 +26,46 @@ protected function createComponentSignInForm(): Form
 		$form->onSuccess[] = [$this, 'signInFormSucceeded'];
 		return $form;
 	}
-public function signInFormSucceeded(Form $form, \stdClass $data): void
-    {
-	try {
-		$this->getUser()->login($data->username, $data->password);
-		$this->redirect('Homepage:');
+	public function signInFormSucceeded(Form $form, \stdClass $data): void
+	{
+		try {
+			$this->getUser()->login($data->username, $data->password);
+			$this->redirect('Homepage:');
+		} catch (Nette\Security\AuthenticationException $e) {
+			$form->addError('IMPOSTER.');
+		}
+	}
+	protected function createComponentSignUpForm(): Form
+	{
+		$form = new Form;
+		$form->addText('username', 'Kultistická přezdívka:')
+			->setRequired('Vytvořte si své uživatelské jméno.');
+		$form->addEmail('email', 'E-mail:')
+			->setRequired('Zadejte váš e-mail.');
+		$form->addPassword('password', 'Heslo:')
+			->setRequired('Vytvořte si své heslo.')
+			->addRule(Form::MIN_LENGTH, 'Minimální délka hesla je %d znaků', 6)
+		;
 
-	    } catch (Nette\Security\AuthenticationException $e) {
-		$form->addError('IMPOSTER.');
-	     }
-    }
-public function actionOut(): void
-    {
-	$this->getUser()->logout();
-	$this->flashMessage('Miss you so much');
-	$this->redirect('Homepage:');
-    }
+		$form->addSubmit('send', 'Potvrdit krví');
+
+		$form->onSuccess[] = [$this, 'signUpFormSucceeded'];
+		return $form;
+	}
+	public function signUpFormSucceeded(Form $form, \stdClass $data): void
+	{
+		try {
+			$this->userfacade->add($data->username, $data->email, $data->password);
+			$this->flashMessage('Úspěšně jste se zaregistrovali.');
+			$this->redirect('Sign:in');			
+		} catch (Nette\Security\AuthenticationException $e) {
+			$form->addError('IMPOSTER.');
+		}
+	}
+	public function actionOut(): void
+	{
+		$this->getUser()->logout();
+		$this->flashMessage('Miss you so much');
+		$this->redirect('Homepage:');
+	}
 }
